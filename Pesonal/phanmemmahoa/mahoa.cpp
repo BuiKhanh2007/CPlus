1487d6e80c3a04cbfbe05850bacfbf79ae2538eb
BuiDuyKhanh2007 <khanhdacodau2007@gmail.com>
2023-10-05 13:34:01 +0700

    ma hoa them button

--- Pesonal/phanmemmahoa/mahoa.cpp
+++ Pesonal/phanmemmahoa/mahoa.cpp
@@ -1,59 +1,76 @@
 #include <SDL2/SDL.h>
-#include <string>
 #include <iostream>
 
-int main(int argc,char* argv[])
-{
-	const int SCREEN_WIDTH = 800;
-	const int SCREEN_HEIGHT = 600;
-	//const std::string WInDOW_TITLE = "Phan mem ma hoa BuiKhanh";
-	
-	if (SDL_Init(SDL_INIT_VIDEO)<0)
-	{
-		std::cout<<"error";
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Window* gWindow = nullptr;
+SDL_Renderer* gRenderer = nullptr;
+
+bool InitializeSDL() {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+		return false;
+	}
+
+	gWindow = SDL_CreateWindow("Button Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (gWindow == nullptr) {
+		std::cerr << "Window creation failed: " << SDL_GetError() << std::endl;
+		return false;
+	}
+
+	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+	if (gRenderer == nullptr) {
+		std::cerr << "Renderer creation failed: " << SDL_GetError() << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+void CloseSDL() {
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+	if (!InitializeSDL()) {
 		return -1;
 	}
-	else std::cout<<"ok";
-	
-	SDL_Window* window = SDL_CreateWindow("Ma hoa by BUIKHANH",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (window==nullptr)
-	{
-		std::cout<<"error";
-	}
-	else std::cout<<"fine\n";
 
-	
-	SDL_Renderer* renderer = SDL_CreateRenderer(window,-1, SDL_RENDERER_ACCELERATED);
-	if (renderer==nullptr)
-	{
-		std::cout<<"error";
-	}
-	else std::cout<<"okle";
-
+	bool quit = false;
 	SDL_Event e;
 
-	bool run = true;
-	while (run)
-	{
-		while (SDL_PollEvent(&e)!=0)
-		{
-			if (e.type==SDL_QUIT)
-			{
-				run=false;
+	while (!quit) {
+		while (SDL_PollEvent(&e) != 0) {
+			if (e.type == SDL_QUIT) {
+				quit = true;
+			}
+
+			// Xử lý sự kiện khi người dùng nhấn chuột
+			if (e.type == SDL_MOUSEBUTTONDOWN) {
+				int mouseX, mouseY;
+				SDL_GetMouseState(&mouseX, &mouseY);
+
+				// Kiểm tra xem chuột có nằm trong vùng của nút không
+				if (mouseX >= 200 && mouseX <= 400 && mouseY >= 200 && mouseY <= 300) {
+					std::cout << "Button Clicked!" << std::endl;
+				}
 			}
 		}
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
+		// Xóa màn hình và vẽ nút
+		SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
+		SDL_RenderClear(gRenderer);
 
+		SDL_Rect buttonRect = {200, 200, 200, 100};
+		SDL_SetRenderDrawColor(gRenderer, 0, 0, 255, 255); // Màu nền xanh
+		SDL_RenderFillRect(gRenderer, &buttonRect); // Vẽ nút
+
+		SDL_RenderPresent(gRenderer); // Hiển thị nút lên màn hình
 	}
 
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	
-
+	CloseSDL();
 	return 0;
 }
