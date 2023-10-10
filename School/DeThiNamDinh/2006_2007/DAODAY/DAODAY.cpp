1567983769b5e0cac72732849ba7970b647202c1
BuiDuyKhanh2007 <khanhdacodau2007@gmail.com>
2023-10-10 13:44:24 +0700

    1:44pm 10/10/2023 bai daoday

--- School/DeThiNamDinh/2006_2007/DAODAY/DAODAY.cpp
+++ School/DeThiNamDinh/2006_2007/DAODAY/DAODAY.cpp
@@ -1,15 +1,29 @@
 #include <bits/stdc++.h>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    int n,d=2000000001;
+    int n,minn=0,d=0;
     cin>>n;
     int khanh[n];
-    cin>>khanh[0];
-    for (int i=1;i<n;i++)
+    int l[n/2],r[n/2]; 
+    for (int i=0;i<n;i++)
     {
-        cin>>khanh[i];
-        d=min(d,khanh[i]+khanh[i-1]);
+        int a;
+        cin>>a;
+        khanh[i]=a;
     }
-    cout<<d;
+    sort(khanh,khanh+n);
+    copy(khanh,khanh+n/2-1,l);
+    copy(khanh,khanh+n,r);
+    sort(r,r+n/2,greater<int>()); 
+    cout<<sizeof(l)/sizeof(l[0])<<" "<<sizeof(r)/sizeof(r[0])<<" ";
+
+    for(int i=0;i<n/2+1;i++)
+    {
+        d=r[i]+l[i];
+        minn=max(minn,d);
+    }
+    cout<<minn;
+
 }
